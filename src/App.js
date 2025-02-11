3e2941a67890a339659d6351097c0c8bf4d14fda
Jerwyn Ponio <jerwynponio13@gmail.com>
2025-02-11 19:49:12 +0800

    attempt push

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+
+    
+  <>
+  <Header />
+
+  <main>
+  <h1>welcome myfirstreactproject</h1>
+  </main>
+
+  <Footer />
+
+  
+  </>
+  );
+}
+
+export default App;
